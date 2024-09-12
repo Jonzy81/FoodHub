cc0083c1cc794f8097f8427c5f2942e16fc9b86b
jonnytouma <jonnytouma@gmail.com>
2024-09-12 13:20:12 +0200

    Added IBookingRepository
    -implemented first operations on interface

---
+++ Data/Repository/IRepository/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using FoodHub.Model;
+
+namespace FoodHub.Data.Repository.IRepository
+{
+    public interface IBookingRepository
+    {
+        Task<IEnumerable<Booking>> GetAllBookingsAsync();
+        Task<Booking> GetBookingByIdAsync(int bookingId);
+        Task AddBookingAsync(Booking booking);
+        Task UpdateBookingAsync(Booking booking);
+        Task DeleteBookingAsync(int bookingId);
+        Task<IEnumerable<Table>> GetAvaliableTablesAsync(DateOnly date, TimeOnly time);
+    }
+}
