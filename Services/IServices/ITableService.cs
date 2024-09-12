585ef69ff647f1ae7b993e36fcf81aed08fd05ee
jonnytouma <jonnytouma@gmail.com>
2024-09-12 13:58:40 +0200

    Finished ItableService

--- Services/IServices/ITableService.cs
+++ Services/IServices/ITableService.cs
@@ -1,7 +1,14 @@
-﻿namespace FoodHub.Services.IServices
+﻿using FoodHub.Model.Dtos;
+
+namespace FoodHub.Services.IServices
 {
     public interface ITableService
     {
-        Task<IEnumerable<>>
+        Task<IEnumerable<TableDto>> GetAlltablesAsync();
+        Task<TableDto> GetTablebyIdAsync(int tableId);
+        Task AddTableAsync(TableDto table);
+        Task UpdatetableAsync(TableDto table);
+        Task DeleteTableAsync(int tableId);
+        Task<bool> IsTableAvailableAsync(int tableId, DateOnly date, TimeOnly time);
     }
 }
