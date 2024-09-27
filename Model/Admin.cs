50c25c180070f252c425419a2bc5b0d7e7c72d39
jonnytouma <jonnytouma@gmail.com>
2024-09-27 13:56:26 +0200

    added new model for admin

---
+++ Model/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodHub.Model
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Passwordhash { get; set; }
+    }
+}
